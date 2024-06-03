3d590776ebad70c3c8d8ee35f96ea5aec03f314a
Matteo Tammaccaro <m.tammaccaro@cloudphoenix.it>
2024-06-03 17:54:35 +0200

    :recycle: Use file-scoped namespace

--- Models/PendingLog.cs
+++ Models/PendingLog.cs
@@ -1,9 +1,10 @@
-﻿namespace NLogFlake.Models
+﻿namespace NLogFlake.Models;
+
+internal class PendingLog
 {
-    internal class PendingLog
-    {
-        public int Retries { get; set; }
-        public string QueueName { get; set; }
-        public string JsonString { get; set; }
-    }
+    public int Retries { get; set; }
+
+    public string? QueueName { get; set; }
+
+    public string? JsonString { get; set; }
 }
