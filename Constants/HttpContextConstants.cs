e4b25028b429436bbe049872cb5a00a4b4795a9b
Matteo Tammaccaro <m.tammaccaro@cloudphoenix.it>
2024-06-03 17:52:15 +0200

    :truck: Move Constants to different files

---
+++ Constants/HttpContextConstants.cs
@@ -0,0 +1,16 @@
+namespace NLogFlake.Constants;
+
+internal static class HttpContextConstants
+{
+    internal const string ParentCorrelationHeader = "parent-correlation";
+
+    internal const string AutoLogGlobalExceptions = "AUTOLOG_GLOBAL_EXCEPTIONS";
+
+    internal const string TraceContext = "TRACE_CONTEXT";
+
+    internal const string ClientId = "clientId";
+
+    internal const string ClientIdOther = "azp";
+
+    internal const string HasCatchedError = "HasCatchedError";
+}
---
+++ Constants/ServersConstants.cs
@@ -0,0 +1,8 @@
+namespace NLogFlake.Constants;
+
+internal static class ServersConstants
+{
+    internal const string PRODUCTION = "https://app.logflake.io";
+
+    internal const string TEST = "https://app-test.logflake.io";
+}
